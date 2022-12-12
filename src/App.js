ef392356a690ae44a56368115802dfe6bab31ef1
Roman <romantimilsina@gmail.com>
2022-12-12 16:25:59 +0545

    Added-Api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,229 @@
+import { useEffect , useState} from 'react';
 import './App.css'
 import CurrencyRow from './CurrencyRow'
+import {v4 as uuidv4} from 'uuid'
 
-const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
+
+export const currencyList = [
+  "EUR",
+  "AED",
+  "AFN",
+  "ALL",
+  "AMD",
+  "ANG",
+  "AOA",
+  "ARS",
+  "AUD",
+  "AWG",
+  "AZN",
+  "BAM",
+  "BBD",
+  "BDT",
+  "BGN",
+  "BHD",
+  "BIF",
+  "BMD",
+  "BND",
+  "BOB",
+  "BRL",
+  "BSD",
+  "BTC",
+  "BTN",
+  "BWP",
+  "BYN",
+  "BYR",
+  "BZD",
+  "CAD",
+  "CDF",
+  "CHF",
+  "CLF",
+  "CLP",
+  "CNY",
+  "COP",
+  "CRC",
+  "CUC",
+  "CUP",
+  "CVE",
+  "CZK",
+  "DJF",
+  "DKK",
+  "DOP",
+  "DZD",
+  "EGP",
+  "ERN",
+  "ETB",
+  "EUR",
+  "FJD",
+  "FKP",
+  "GBP",
+  "GEL",
+  "GGP",
+  "GHS",
+  "GIP",
+  "GMD",
+  "GNF",
+  "GTQ",
+  "GYD",
+  "HKD",
+  "HNL",
+  "HRK",
+  "HTG",
+  "HUF",
+  "IDR",
+  "ILS",
+  "IMP",
+  "INR",
+  "IQD",
+  "IRR",
+  "ISK",
+  "JEP",
+  "JMD",
+  "JOD",
+  "JPY",
+  "KES",
+  "KGS",
+  "KHR",
+  "KMF",
+  "KPW",
+  "KRW",
+  "KWD",
+  "KYD",
+  "KZT",
+  "LAK",
+  "LBP",
+  "LKR",
+  "LRD",
+  "LSL",
+  "LTL",
+  "LVL",
+  "LYD",
+  "MAD",
+  "MDL",
+  "MGA",
+  "MKD",
+  "MMK",
+  "MNT",
+  "MOP",
+  "MRO",
+  "MUR",
+  "MVR",
+  "MWK",
+  "MXN",
+  "MYR",
+  "MZN",
+  "NAD",
+  "NGN",
+  "NIO",
+  "NOK",
+  "NPR",
+  "NZD",
+  "OMR",
+  "PAB",
+  "PEN",
+  "PGK",
+  "PHP",
+  "PKR",
+  "PLN",
+  "PYG",
+  "QAR",
+  "RON",
+  "RSD",
+  "RUB",
+  "RWF",
+  "SAR",
+  "SBD",
+  "SCR",
+  "SDG",
+  "SEK",
+  "SGD",
+  "SHP",
+  "SLE",
+  "SLL",
+  "SOS",
+  "SRD",
+  "STD",
+  "SVC",
+  "SYP",
+  "SZL",
+  "THB",
+  "TJS",
+  "TMT",
+  "TND",
+  "TOP",
+  "TRY",
+  "TTD",
+  "TWD",
+  "TZS",
+  "UAH",
+  "UGX",
+  "USD",
+  "UYU",
+  "UZS",
+  "VEF",
+  "VES",
+  "VND",
+  "VUV",
+  "WST",
+  "XAF",
+  "XAG",
+  "XAU",
+  "XCD",
+  "XDR",
+  "XOF",
+  "XPF",
+  "YER",
+  "ZAR",
+  "ZMK",
+  "ZMW",
+  "ZWL"
+];
+
+
+const old_api = "Ry4IiQHCUOuEF6OstB5noWHBNJiEIQix";
 
 function App() {
+  
+  const [currencyOptions,setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
+ 
+  
+
+
+  useEffect(() => {
+    
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "0uuFfUpv9jqtfBhdO7WQVn9fOQRbSLsV");
+  
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+    fetch(`https://api.currencyfreaks.com/latest?apikey=abb36491c2d04d02a73898c5888db206`)
+  .then(response => response.json())
+  .then(result => {
+    setCurrencyOptions([result.base,...Object.keys(result.rates)])
+
+  }
+  )
+ 
+  },[])
+
+  
+  
+
+
   return (
     <>
     <h1> Convert </h1>
-    <CurrencyRow />
+    <select>
+   {currencyOptions.map(option => (
+        <option key={uuidv4()} value={option}>{option}</option>
+        ))}
+    </select>
     <div className='equals'>=</div>
-    <CurrencyRow />
+    
     </>
     
   );
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+  
   return (
     <>
     <input type= "number" className="input"/>
-    <select>
-      <option value = 'Hi' >Hi</option>
-    </select>
+   
     </>
   )
 }
